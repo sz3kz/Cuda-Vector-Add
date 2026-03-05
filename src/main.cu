df3ac02d21af98e0a40efe9615c99fb1903ad47a
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-03-05 08:47:33 +0100

    Pseudo-transfer vectors from host to device

--- src/main.cu
+++ src/main.cu
@@ -16,9 +16,23 @@ int main(){
   vectorPrint("Vector B", vector_B, VECTOR_LENGTH);
   vectorPrint("Vector C", vector_C, VECTOR_LENGTH);
 
+  int * vector_gpu_A, * vector_gpu_B, * vector_gpu_C;
+
+  cudaMalloc(&vector_gpu_A, sizeof(int) * VECTOR_LENGTH);
+  cudaMalloc(&vector_gpu_B, sizeof(int) * VECTOR_LENGTH);
+  cudaMalloc(&vector_gpu_C, sizeof(int) * VECTOR_LENGTH);
+
+  cudaMemcpy(vector_gpu_A, vector_A, sizeof(int) * VECTOR_LENGTH, cudaMemcpyHostToDevice);
+  cudaMemcpy(vector_gpu_B, vector_B, sizeof(int) * VECTOR_LENGTH, cudaMemcpyHostToDevice);
+  cudaMemcpy(vector_gpu_C, vector_C, sizeof(int) * VECTOR_LENGTH, cudaMemcpyHostToDevice);
+
   delete[] vector_A;
   delete[] vector_B;
   delete[] vector_C;
+
+  cudaFree(vector_gpu_A);
+  cudaFree(vector_gpu_B);
+  cudaFree(vector_gpu_C);
 
   return 0;
 }
